1b97891c0e8801684d796ff256604401b368b4ac
gsnyder206 <snyder.gregoryf@gmail.com>
2016-07-12 09:16:41 -0400

    resources

--- panstarrs/check_mock_efficiency.py
+++ panstarrs/check_mock_efficiency.py
@@ -10,14 +10,17 @@ if __name__=="__main__":
     out_files = np.sort(np.asarray(glob.glob("group_15??_pbs.out")))
     total_wall_time_mins=0.0
     total_cpu_time_mins=0.0
+    SUs_list = []
+    eff_list = []
+    cpu_mins_list = []
+    wall_mins_list = []
     
     for of in out_files:
         with open(of,'r') as F:
             for line in F:
                 if line.find('Resources Used') is not -1:
                     cpus = line[21:29]
-                    walls = line[-9:-1]
-                    print of, cpus, walls
+                    walls = line[-9:-1]  #account for \n
                     
                     cputs = time.strptime(cpus,"%H:%M:%S")
                     wallts = time.strptime(walls,"%H:%M:%S")
@@ -25,5 +28,19 @@ if __name__=="__main__":
                     wall_mins = wallts.tm_hour*60.0 + wallts.tm_min + wallts.tm_sec/60.0
                     SUs = 16.0*wall_mins/60.0
                     eff = cpu_mins/(wall_mins*16.0)
+                    total_wall_time_mins = total_wall_time_mins +wall_mins
+                    total_cpu_time_mins = total_cpu_time_mins +cpu_mins
+                    SUs_list.append(SUs)
+                    eff_list.append(eff)
+                    cpu_mins_list.append(cpu_mins)
+                    wall_mins_list.append(wall_mins)
+                    
                     print of, SUs, eff, cpu_mins, wall_mins
                     
+
+    print "Mean efficiency: ", total_cpu_time_mins/(total_wall_time_mins*16.0)
+
+    
+    for i in range(SUs_list):
+        print SUs_list[i], eff_list[i], cpu_mins_list[i], wall_mins_list[i]
+        
