470a8ae82dcbb99bd5f4c5696d0e222410d47dcc
gsnyder206 <snyder.gregoryf@gmail.com>
2016-07-12 08:54:43 -0400

    resources

---
+++ panstarrs/check_mock_efficiency.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+import glob
+import numpy as np
+import astropy.io.ascii as ascii
+
+
+if __name__=="__main__":
+    out_files = np.sort(np.asarray(glob.glob("group_15??_pbs.out")))
+    total_wall_time_mins=0.0
+    total_cpu_time_mins=0.0
+    
+    for of in out_files:
+        with open(of,'r') as F:
+            print F[7]
