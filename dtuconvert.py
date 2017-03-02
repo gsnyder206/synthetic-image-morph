17a7f49a5212f887396f0b2c41cebfd99b47d75d
gsnyder206 <gsnyder@stsci.edu>
2017-03-02 07:36:20 -0500

    hack

--- dtuconvert.py
+++ dtuconvert.py
@@ -14,17 +14,35 @@ import os
 
 import time
 
+import h5py
 
-def convert_galaxy(filename,outfilename):
+def convert_galaxy(filename,outfilename,sfid_all,merger_bool_all):
     print(filename,outfilename)
+
+    hdu = pyfits.open(filename)[0]
+
     array = pyfits.open(filename)[0].data
-    new_array = congrid.congrid(array,(64,64))
+    new_array = congrid.congrid(array[61:162,61:162],(64,64))
     
+    sfid = float( hdu.header['SUBH_ID'] )
+
+    #mbi = np.where(sfid_all==sfid)[0]
+    #mb = merger_bool_all[mbi]
+    #print(mb,mbi,sfid_all)
+
+    index = sfid_all==sfid
+
+    mb = merger_bool_all[sfid_all==sfid]
+
     fu = pyfits.PrimaryHDU(new_array)
+
+    fu.header['SUBH_ID']=sfid
+    fu.header['ISMERGER']=float(mb)
+
     ful = pyfits.HDUList([fu])
 
     ful.writeto(outfilename,overwrite=True)
-
+    
 
     return
 
@@ -33,7 +51,17 @@ def convert_galaxy(filename,outfilename):
 def convert_galaxies(sd='subdir_010'):
     image_base='/astro/snyder_lab/Illustris_CANDELS/Illustris-1_z1_images_bc03/snapshot_068/'
 
-    
+    morphology_catalog_file = 'morphology_catalog_068.hdf5'
+
+    with h5py.File(morphology_catalog_file,'r') as catfile:
+        snap_properties = catfile['snapshot_068']
+        
+        #Now, an intrinsic quantity that we want to learn about
+        #Is there a major merger within +/- 500Myr ?
+        merger_bool= np.asarray( catfile['snapshot_068']['Mergers_About1Gyr'].value)
+        sfid = np.asarray( catfile['snapshot_068']['SubfindID'].value )
+        print(sfid,merger_bool)
+
 
     files_105 = np.sort(np.asarray(glob.glob(image_base+str(sd)+'/images_subhalo_*/*WFC3-F105W_SB00.fits')))
     files_125 = np.sort(np.asarray(glob.glob(image_base+str(sd)+'/images_subhalo_*/*WFC3-F125W_SB00.fits')))
@@ -47,15 +75,15 @@ def convert_galaxies(sd='subdir_010'):
 
     for f in files_105:
         outfilename=sd+'/rebin_'+os.path.basename(f)
-        convert_galaxy(f,outfilename)
+        convert_galaxy(f,outfilename,sfid,merger_bool)
  
     for f in files_125:
         outfilename=sd+'/rebin_'+os.path.basename(f)
-        convert_galaxy(f,outfilename)
+        convert_galaxy(f,outfilename,sfid,merger_bool)
         
     for f in files_160:
         outfilename=sd+'/rebin_'+os.path.basename(f)
-        convert_galaxy(f,outfilename)
+        convert_galaxy(f,outfilename,sfid,merger_bool)
                
 
     return
@@ -67,6 +95,8 @@ if __name__=="__main__":
 
     st = time.time()
 
+    convert_galaxies(sd='subdir_000')
+    
     convert_galaxies(sd='subdir_001')
     convert_galaxies(sd='subdir_002')
     convert_galaxies(sd='subdir_003')
