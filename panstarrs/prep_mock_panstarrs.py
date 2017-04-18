df27658b05d745b5ad0012ec0d1ec96253578b12
gsnyder206 <snyder.gregoryf@gmail.com>
2017-04-18 13:52:57 -0400

    setup mock panstarrs

--- panstarrs/prep_mock_panstarrs.py
+++ panstarrs/prep_mock_panstarrs.py
@@ -1,26 +1,35 @@
 #https://github.com/gsnyder206/mock-surveys
 import illustris_api_utils as iau
 import illustris_sunrise_utils as isu
+### system modules
+import os
 
 
-def prep_mock_panstarrs():
+def prep_mock_panstarrs(input_catalog):
     sim='Illustris-1'
-    savepath=os.path.expandvars('$HOME/oasis_project_hsc102/IllustrisData/')
+    savepath=os.path.expandvars('$IllustrisData')  #want it at /oasis/projects/nsf/hsc102/vrg/IllustrisData/ maybe?  
     use_z = 0.05
     
     #select snapshots and subhalos using web API locally or catalogs at STScI or harvard
+    #e.g., based on input_catalog file, need snapshot number and SubfindID number
 
-
+    #e.g.
+    data=ascii.read(input_catalog)
+    sn=data['snap']
+    sfid=data['sfid']
+    
+    
     #loop over selected objects:
+    for this_sn,this_sfid in zip(sn,sfid):
 
-    for galaxy,sn,this_sfid in (selection object):
-        
-        f,s,d=iau.get_subhalo(sim,sn,this_sfid,savepath=savepath,verbose=True,clobber=False)
+        #this checks if halo exists, downloads it if not, and converts into Sunrise-readable format
+        f,s,d=iau.get_subhalo(sim,this_sn,this_sfid,savepath=savepath,verbose=True,clobber=False,get_parent=True)  
 
-        #may want to create new function isu.setup_sunrise_illustris_panstarrs(f,s,redshift_override=use_z,filters='$MOCK_SURVEYS/tng/filters_lsst_light.txt')  ?
+        #may want to create new functions based around setup_sunrise_illustris_panstarrs(f,s,redshift_override=use_z,filters='$MOCK_SURVEYS/tng/filters_lsst_light.txt')  ?
         isu.setup_sunrise_illustris_subhalo(f,s,redshift_override=use_z)
         #this also needs to be edited to include the candelization step in the job scripts, and output job submission scripts a la the lightcone function in "isu" module.
-    
+
+        #the result will be all necessary snapshot data plus ancillary Sunrise data and input files, plus submission scripts
 
 
 
