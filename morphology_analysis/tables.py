71332202d686723aeb9381c4fbe73fb4181e4f21
gsnyder206 <gsnyder@stsci.edu>
2018-05-15 13:31:24 -0400

    long overdue update

---
+++ morphology_analysis/tables.py
@@ -0,0 +1,12 @@
+import astropy
+import illcan_multiplots as icmp
+
+
+
+if __name__=="__main__":
+    snapshots=['103','085','075','068','064','060','054','049','045','041','038','035']
+    redshifts=[  0.5,  1.0,  1.5,  2.0,  2.5,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0,  9.0]
+    ngalaxies=[]
+
+
+    
